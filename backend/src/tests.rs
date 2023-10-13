c836d37c64731a812181886ce96699510e7b6e50
ErmitaVulpe <tymon.wilczek@gmail.com>
2023-10-14 01:29:53 +0200

    Created initial backend files

---
+++ backend/src/tests.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn add_ints() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn add_strings() {
+        let a = "Hello ".to_string();
+        let b = a + "world!";
+        assert_eq!(b, "Hello world!");
+    }
+}
+
+/*
+# cargo test
+   Compiling backend v0.1.0 (/root/projects/flashcard/backend)
+    Finished test [unoptimized + debuginfo] target(s) in 0.24s
+     Running unittests src/main.rs (target/debug/deps/backend-1dfcf1f8ad3adfc6)
+
+running 2 tests
+test tests::tests::add_ints ... ok
+test tests::tests::add_strings ... ok
+
+test result: ok. 2 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+*/
