ebbd5b71076fb41227126ba132e88c134522a53c
Togeee12 <stach.szymczykowski@gmail.com>
2023-10-15 20:37:34 +0200

    update frontend

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset', // Vue CLI preset
-    '@babel/preset-typescript',     // TypeScript preset
-    [
-      '@babel/preset-env',
-      {
-        modules: false, // Use 'false' to target ES modules
-      },
-    ],
-  ],
-};
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
