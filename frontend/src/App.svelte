2ccccf18912fe050aed34273aad2e5f50523d3ea
Togeee12 <stach.szymczykowski@gmail.com>
2023-10-21 23:58:33 +0200

    Frontend update

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,19 @@
 <script>
   import Flashcard from "./Flashcard.svelte";
+
+  import Menu from "./Menu.svelte";
+
+  let menuItems = [
+    { label: 'Home', link: '/' },
+    { label: 'About', link: '/about' },
+    { label: 'Services', link: '/services' },
+    { label: 'Contact', link: '/contact' }
+  ];
 </script>
 
+
+<Menu {menuItems} />
 <Flashcard />
+
+
+
---
+++ frontend/src/Menu.svelte
@@ -0,0 +1,58 @@
+<script>
+    import { listen } from "svelte/internal";
+
+    export let menuItems = [];
+
+    function navigate(link) {
+        console.log("Navigating to link: {link}")
+    }
+
+</script>
+
+<nav>
+    <ul>
+        {#each menuItems as item (item.label)}
+      <li>
+        <a on:click={() => navigate(item.link)}>{item.label}</a>
+      </li>
+    {/each}
+    </ul>
+</nav>
+
+
+<style>
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+nav {
+    background-color: #eee;
+    padding: 10px; 
+}
+
+ul {
+    display: flex; 
+    justify-content: space-between; 
+}
+
+li {
+    margin: 0 15px;     
+
+}
+
+a {
+    text-decoration: none; 
+    color: #fff;
+    font-weight: bold; 
+    transition: color 0.3s;
+    color: #42004b;
+}
+
+a:hover {
+    color: #6e007c; 
+    cursor: pointer;
+}
+
+</style>
